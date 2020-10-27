1ca0f1469563ddb78e541ee93ceb82397ec51b15
QuangNam <quangnamhd9x@gmail.com>
2020-10-27 09:52:20 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PetComponent } from './pet/pet.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PetComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
